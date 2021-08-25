dcb280cf5ed6b4a36d91df272dc158896b2df1f0
William <68653853+will-i-amv@users.noreply.github.com>
2021-08-25 15:46:16 -0400

    Remove type annotations

--- CH3/ch03_ex1.py
+++ CH3/ch03_ex1.py
@@ -8,18 +8,17 @@ Chapter 3, Example Set 1
 # Callable objects as higher order functions
 ###########################################
 
-from typing import Callable
 
 class Mersenne1:
     """Callable object with a **Strategy** plug in required."""
-    def __init__(self, algorithm: Callable[[int], int]) -> None:
+    def __init__(self, algorithm) :
         self.pow2 = algorithm
-    def __call__(self, arg: int) -> int:
+    def __call__(self, arg):
         return self.pow2(arg) - 1
 
 # Functions that implement 2 ^ b
 
-def shifty(b: int) -> int:
+def shifty(b):
     """2**b via shifting.
 
     >>> shifty(17)-1
@@ -28,7 +27,7 @@ def shifty(b: int) -> int:
     return 1 << b
 
 
-def multy(b: int) -> int:
+def multy(b):
     """2**b via naive recursion.
 
     >>> multy(17)-1
@@ -38,7 +37,7 @@ def multy(b: int) -> int:
     return 2*multy(b-1)
 
 
-def faster(b: int) -> int:
+def faster(b):
     """2**b via faster divide-and-conquer recursion.
 
     >>> faster(17)-1
